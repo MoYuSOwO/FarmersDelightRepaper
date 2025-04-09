3d34bde28fc978710d01cf4769555ebe519f3c45
MoYuSOwO <hhj2006@outlook.com>
2025-04-09 20:43:44 +0800

    Initial commit

--- plugin/src/main/java/io/github/MoYuSOwO/FarmersDelightRepaperPlugin/CuttingRecipe.java
+++
@@ -1,8 +0,0 @@
-package io.github.MoYuSOwO.FarmersDelightRepaperPlugin;
-
-import org.bukkit.inventory.ItemStack;
-
-public record CuttingRecipe(
-        ItemStack input,
-        ItemStack[] output
-) {}
---
+++ src/main/java/io/github/MoYuSOwO/farmersDelightRepaper/FarmersDelightRepaper.java
@@ -0,0 +1,17 @@
+package io.github.MoYuSOwO.farmersDelightRepaper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FarmersDelightRepaper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
